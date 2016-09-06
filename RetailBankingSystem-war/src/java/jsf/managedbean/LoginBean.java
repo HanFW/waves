ddc1d3280e9ce71efb87a606408090b174b2787f
Han Fengwei <hanfengwei@192.168.0.103>
2016-09-06 17:42:14 +0800

    add logged in customer to the session

--- RetailBankingSystem-war/src/java/jsf/managedbean/LoginBean.java
+++ RetailBankingSystem-war/src/java/jsf/managedbean/LoginBean.java
@@ -5,71 +5,77 @@
  */
 package jsf.managedbean;
 
+import entity.CustomerBasic;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.inject.Named;
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import org.primefaces.context.RequestContext;
 import session.stateless.AdminSessionBeanLocal;
+
 /**
  *
  * @author hanfengwei
  */
 @Named(value = "loginBean")
-@RequestScoped
-public class LoginBean {
+@SessionScoped
+public class LoginBean implements Serializable {
+
     @EJB
     private AdminSessionBeanLocal adminSessionBeanLocal;
-    
+
     private String customerAccount;
     private String customerPassword;
-    
+    private CustomerBasic customer;
+
     /**
      * Creates a new instance of LoginBean
      */
     public LoginBean() {
     }
-    
+
     /**
      *
      * @param event
      */
-    public void doLogin(ActionEvent event){
-        adminSessionBeanLocal.createOnlineBankingAccount(Long.valueOf(4));
-        
-        RequestContext context = RequestContext.getCurrentInstance();
+    public void doLogin(ActionEvent event) {
+//        adminSessionBeanLocal.createOnlineBankingAccount(Long.valueOf(5));
+
         FacesMessage message = null;
+        FacesContext context = FacesContext.getCurrentInstance();
+        
         //encrypt the customerPassword first
-        String status = adminSessionBeanLocal.login(customerAccount,customerPassword);
-        String redirect;
+        String status = adminSessionBeanLocal.login(customerAccount, customerPassword);   
         switch (status) {
             case "loggedIn":
-                message = new FacesMessage(FacesMessage.SEVERITY_INFO, status, "Welcome back!");
-                redirect = "home.xhtml";
+//                message = new FacesMessage(FacesMessage.SEVERITY_INFO, status, "Welcome back!");
                 System.out.println("*** loginBean: loggedIn");
+                customer = adminSessionBeanLocal.getCustomerByOnlineBankingAccount(customerAccount);
+                context.getExternalContext().getSessionMap().put("customer", customer);
+                try {
+                    context.getExternalContext().redirect("home.xhtml");
+                } catch (IOException ex) {
+                    Logger.getLogger(LoginBean.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                System.out.println("******************* test sessionMap");
+                System.out.println(context.getExternalContext().getSessionMap().get("customer"));
                 break;
             case "invalidPassword":
                 message = new FacesMessage(FacesMessage.SEVERITY_INFO, status, "Invalid customerPassword/account.");
-                redirect = "login.xhtml";
+                context.addMessage(null, message);
                 System.out.println("*** loginBean: invalid password");
                 break;
             default:
                 message = new FacesMessage(FacesMessage.SEVERITY_INFO, status, "Please check your account number.");
-                redirect = "login.xhtml";
+                context.addMessage(null, message);
                 System.out.println("*** loginBean: invalid account");
                 break;
-        }
-        FacesContext.getCurrentInstance().addMessage(null, message);
-
-        try {
-            FacesContext.getCurrentInstance().getExternalContext().redirect(redirect);
-        } catch (IOException ex) {
-            Logger.getLogger(LoginBean.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
@@ -99,5 +105,19 @@ public class LoginBean {
      */
     public void setCustomerPassword(String customerPassword) {
         this.customerPassword = customerPassword;
+    }
+
+    /**
+     * @return the customer
+     */
+    public CustomerBasic getCustomer() {
+        return customer;
+    }
+
+    /**
+     * @param customer the customer to set
+     */
+    public void setCustomer(CustomerBasic customer) {
+        this.customer = customer;
     }
 }
