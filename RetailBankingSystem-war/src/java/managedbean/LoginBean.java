bfea37332e8f9ae3c4d9bfbd33cca76eff8acd7a
Han Fengwei <hanfengwei@192.168.0.102>
2016-09-12 11:35:24 +0800

    add MerlionBankingInternalSystem web applicaiton
    need to add another server in order for two web applications to run without conflicts
    added dummy files in new folders, please delete after creating new files

--- RetailBankingSystem-war/src/java/managedbean/LoginBean.java
+++ RetailBankingSystem-war/src/java/managedbean/LoginBean.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jsf.managedbean;
+package managedbean;
 
 import entity.CustomerBasic;
 import java.io.IOException;
