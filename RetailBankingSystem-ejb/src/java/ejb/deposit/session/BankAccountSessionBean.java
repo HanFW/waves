cc946e5642bdda5be33dff1dbddcfd5ab4882db0
Yongxue <Yongxue@192.168.0.101>
2016-09-23 22:00:12 +0800

    add customer forgot password

--- RetailBankingSystem-ejb/src/java/ejb/deposit/session/BankAccountSessionBean.java
+++ RetailBankingSystem-ejb/src/java/ejb/deposit/session/BankAccountSessionBean.java
@@ -1,11 +1,13 @@
 package ejb.deposit.session;
 
-import ejb.customer.session.CRMCustomerSessionBean;
 import ejb.infrastructure.session.CustomerAdminSessionBeanLocal;
 import ejb.deposit.entity.BankAccount;
 import ejb.deposit.entity.AccTransaction;
 import ejb.customer.entity.CustomerBasic;
+import ejb.customer.session.CRMCustomerSessionBeanLocal;
 import ejb.deposit.entity.Interest;
+import ejb.infrastructure.session.CustomerAdminSessionBean;
+import ejb.infrastructure.session.CustomerEmailSessionBeanLocal;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -15,11 +17,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
@@ -32,6 +36,9 @@ import javax.persistence.NonUniqueResultException;
 public class BankAccountSessionBean implements BankAccountSessionBeanLocal {
 
     @EJB
+    private CRMCustomerSessionBeanLocal customerSessionBeanLocal;
+
+    @EJB
     private CustomerAdminSessionBeanLocal adminSessionBeanLocal;
 
     @EJB
@@ -39,9 +46,6 @@ public class BankAccountSessionBean implements BankAccountSessionBeanLocal {
 
     @EJB
     private InterestSessionBeanLocal interestSessionLocal;
-
-    @EJB
-    private CRMCustomerSessionBean customerSessionBean;
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -90,7 +94,7 @@ public class BankAccountSessionBean implements BankAccountSessionBeanLocal {
 
     @Override
     public List<BankAccount> retrieveBankAccountByCusIC(String customerIdentificationNum) {
-        CustomerBasic customerBasic = customerSessionBean.retrieveCustomerBasicByIC(customerIdentificationNum.toUpperCase());
+        CustomerBasic customerBasic = customerSessionBeanLocal.retrieveCustomerBasicByIC(customerIdentificationNum.toUpperCase());
 
         if (customerBasic.getCustomerBasicId() == null) {
             return new ArrayList<BankAccount>();
@@ -566,12 +570,12 @@ public class BankAccountSessionBean implements BankAccountSessionBeanLocal {
 
         return customerBasic;
     }
-    
+
     @Override
     public void updatePwd(String bankAccountNum, String bankAccountPwd) {
-        
+
         BankAccount bankAccount = retrieveBankAccountByNum(bankAccountNum);
-        
+
         String hashedPwd = "";
 
         try {
@@ -579,7 +583,7 @@ public class BankAccountSessionBean implements BankAccountSessionBeanLocal {
         } catch (NoSuchAlgorithmException ex) {
             Logger.getLogger(BankAccountSessionBean.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         bankAccount.setBankAccountPwd(hashedPwd);
     }
 }
