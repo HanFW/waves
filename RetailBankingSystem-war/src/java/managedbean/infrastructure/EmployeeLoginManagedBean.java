464d91749ba5b004d737b8e3ffbdf6931b9a5b8f
tianxiejojo <tianxiezjy@gmail.com>
2016-09-25 21:24:32 +0800

    Merge branch 'U1-infrastructure' of https://github.com/HanFW/waves into U1-infrastructure
    * 'U1-infrastructure' of https://github.com/HanFW/waves:
      update delete and reopen online banking account functions
      add delete and reopen online banking account functions
      half finish delete online banking account
      Merge branch 'U2-customerMS'
      update open account jsf
      add customer message box
      Department Specialists can reply issues of their own department. RM can view and edit customer advanced profile. Or also search customer using filter. counter teller can search customer by IC and edit basic info or write enquiry
      update open account managedbean for customer and employee
      add verify process to employee open account
    
    # Conflicts:
    #	RetailBankingSystem-war/src/java/managedbean/infrastructure/EmployeeLoginManagedBean.java

--- RetailBankingSystem-war/src/java/managedbean/infrastructure/EmployeeLoginManagedBean.java
+++ RetailBankingSystem-war/src/java/managedbean/infrastructure/EmployeeLoginManagedBean.java
@@ -16,8 +16,6 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
-import javax.persistence.Query;
-import ejb.infrastructure.session.CustomerAdminSessionBeanLocal;
 import ejb.infrastructure.session.EmployeeAdminSessionBeanLocal;
 import javax.faces.context.ExternalContext;
 import ejb.infrastructure.session.LoggingSessionBeanLocal;
