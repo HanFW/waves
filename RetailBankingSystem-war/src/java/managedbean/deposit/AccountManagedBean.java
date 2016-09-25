16e6a5d814cb2a7fe4cff18ac8c66cd871ecb856
Yongxue <Yongxue@192.168.0.101>
2016-09-26 05:20:00 +0800

    update open account message

--- RetailBankingSystem-war/src/java/managedbean/deposit/AccountManagedBean.java
+++ RetailBankingSystem-war/src/java/managedbean/deposit/AccountManagedBean.java
@@ -868,7 +868,10 @@ public class AccountManagedBean implements Serializable {
                         subject = "Welcome to Merlion Bank";
                         Calendar cal = Calendar.getInstance();
                         receivedDate = cal.getTime();
-                        messageContent = "Welcome to Merlion Bank! Please deposit/transfer sufficient fund to your bank account.";
+                        messageContent = "Welcome to Merlion Bank! Please deposit/transfer sufficient fund to activate your bank account.\n "
+                                + "For fixed depsit account, please declare your fixed deposit period first before activating your account.\n"
+                                + "Your daily transfer limit is S$3000. You could update your daily transfer limit under 'My Account'.\n"
+                                + "If you have any enquiry, please contact us at 800 820 8820 or you can write an enquiry after you login.\n";
                         messageSessionBeanLocal.sendMessage("Merlion Bank", "Service", subject, receivedDate.toString(),
                                 messageContent, customerBasicId);
                         loggingSessionBeanLocal.createNewLogging("system", customerBasic.getCustomerBasicId(), "send welcome message", "successful",null);
