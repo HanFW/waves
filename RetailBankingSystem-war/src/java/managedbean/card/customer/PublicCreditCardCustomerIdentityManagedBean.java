2071abc067c62c5ded4739fb549519df6376318a
aaa <aaa@192.168.0.103>
2016-10-18 00:16:04 +0800

    are you existing customer? radio button added

---
+++ RetailBankingSystem-war/src/java/managedbean/card/customer/PublicCreditCardCustomerIdentityManagedBean.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbean.card.customer;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author aaa
+ */
+@Named(value = "publicCreditCardCustomerIdentityManagedBean")
+@RequestScoped
+public class PublicCreditCardCustomerIdentityManagedBean {
+
+    private String hasMerlionAcc;
+    private boolean existingCustomerVisible;
+    private boolean newCustomerVisible;
+    
+    public PublicCreditCardCustomerIdentityManagedBean() {
+    }
+    
+    public void showPanal() {
+        if (hasMerlionAcc.equals("Yes")) {
+            existingCustomerVisible = true;
+        }
+        if (hasMerlionAcc.equals("No")) {
+            newCustomerVisible = true;
+        }
+    }
+
+    public String getHasMerlionAcc() {
+        return hasMerlionAcc;
+    }
+
+    public void setHasMerlionAcc(String hasMerlionAcc) {
+        this.hasMerlionAcc = hasMerlionAcc;
+    }
+
+    public boolean isExistingCustomerVisible() {
+        return existingCustomerVisible;
+    }
+
+    public void setExistingCustomerVisible(boolean existingCustomerVisible) {
+        this.existingCustomerVisible = existingCustomerVisible;
+    }
+
+    public boolean isNewCustomerVisible() {
+        return newCustomerVisible;
+    }
+
+    public void setNewCustomerVisible(boolean newCustomerVisible) {
+        this.newCustomerVisible = newCustomerVisible;
+    }
+    
+}
