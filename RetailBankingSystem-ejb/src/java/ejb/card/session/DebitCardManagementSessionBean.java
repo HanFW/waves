67c67f650c33ab1548a00966933139ee77d2e6df
tianxiejojo <tianxiezjy@gmail.com>
2016-11-06 14:15:48 +0800

    bug fixed -employee & customer debit card display message

--- RetailBankingSystem-ejb/src/java/ejb/card/session/DebitCardManagementSessionBean.java
+++ RetailBankingSystem-ejb/src/java/ejb/card/session/DebitCardManagementSessionBean.java
@@ -50,14 +50,14 @@ public class DebitCardManagementSessionBean implements DebitCardManagementSessio
                 if (!findDebitCard.getDebitCardPwd().equals(hashedInputPwd)) {
                     return "wrong pwd";
                 } else {
-//                    BankAccount depositAccount = findDebitCard.getBankAccount();
-//                    DebitCardType debitCardType = findDebitCard.getDebitCardType();
-//
-//                    depositAccount.removeDebitCard(findDebitCard);
-//                    debitCardType.removeDebitCard(findDebitCard);
-//
-//                    em.remove(findDebitCard);
-                    findDebitCard.setStatus("cancel");
+                    BankAccount depositAccount = findDebitCard.getBankAccount();
+                    DebitCardType debitCardType = findDebitCard.getDebitCardType();
+
+                    depositAccount.removeDebitCard(findDebitCard);
+                    debitCardType.removeDebitCard(findDebitCard);
+
+                    em.remove(findDebitCard);
+//                    findDebitCard.setStatus("cancel");
                     em.flush();
                     return "success";
 
