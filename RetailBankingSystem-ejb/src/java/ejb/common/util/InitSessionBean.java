6cf9cc7c813213497a3ee1a367d9c09fd8e3f9f4
Han Fengwei <hanfengwei96@gmail.com>
2019-04-09 17:12:18 +0800

    test branch contributions

--- RetailBankingSystem-ejb/src/java/ejb/common/util/InitSessionBean.java
+++ RetailBankingSystem-ejb/src/java/ejb/common/util/InitSessionBean.java
@@ -16,7 +16,7 @@ public class InitSessionBean {
 
     @PostConstruct
     public void init() {
-       ejbTimerSessionLocal.createTimer10000MS();
+//        ejbTimerSessionLocal.createTimer10000MS();
 //        ejbTimerSessionLocal.createTimer300000MS();
 //        ejbTimerSessionLocal.createTimer15000MS();
 //        ejbTimerSessionLocal.createTimer70000MS();
